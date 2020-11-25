e3d358d1b6178f32b793228aafae46907572e53d
akshat1032 <akshat.30oct@gmail.com>
2020-11-25 12:18:47 +0530

    Add : Return JSON for different messages using greeting controller added

---
+++ src/main/java/com/springapp/Greeting.java
@@ -0,0 +1,21 @@
+package com.springapp;
+
+public class Greeting {
+	private long id;
+	private String message;
+	
+	public Greeting(long id, String message) {
+		this.id = id;
+		this.message = message;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
+}
